868ebd10d34e88cd3b5436d19935deab3db544d2
Abdulquadri-Mahmud <abdulquadrimahmud06@gmail.com>
2024-09-25 11:58:06 +0100

    changes made

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,12 +7,6 @@ import "slick-carousel/slick/slick-theme.css";
 
 import Slider from "react-slick";
 
-// import side_banner2 from '/img1.png';
-// import side_banner3 from '/img2.png';
-// import side_banner4 from '/img4.png';
-// import side_banner5 from '/side_banner1.png';
-// import side_banner6 from '/img3.png';
-
 import { Link } from 'react-router-dom';
 import { MdOutlineShoppingCart } from 'react-icons/md';
 
@@ -71,48 +65,22 @@ export default function Hero() {
           </div>
           <div className="">
             <p className='text-center md:text-start font-medium'>Shop What You Desire On ADEXIFY</p>
+            <p className='text-center md:text-start font-medium'>We are here to save you time and money</p>
           </div>
           <Flex justifyContent={{md:'start', base: 'center'}}>
             <Button bgGradient='linear(to-l, gray.800, pink.800)' color={'white'} border={'none'} outline={'none'} _hover={{bgGradient:'linear(to-r, gray.800, pink.800)'}} transitionDuration={'0.3s'} className='uppercase bg-pink-600 text-white font-medium px-6 py-2 rounded-md'>shop now</Button>
           </Flex>
         </Flex>
-        {/* <div className="xl:w-[60%] bg-white w-full rounded-lg">
-          <Slider {...settings}>
-            <div className="flex justify-center items-center w-full bg-white  rounded-lg">
-              <img src={side_banner5} alt="" className='md:h-[350px] h-[250px] max-w-full drop-shadow-[0_35px_35px_#0007]  rounded-lg' />
-            </div>
-            <div className="w-full bg-white rounded-lg p-3">
-              <Flex justifyContent={'space-between'} alignItems={'center'}>
-                <div className="">
-                  <img src={side_banner2} alt="" className='md:h-[350px] h-[200px] max-w-full drop-shadow-[0_35px_35px_#0007]  rounded-lg' />
-                </div>
-                <div className="">
-                  <img src={side_banner3} alt="" className='md:h-[350px] h-[200px] max-w-full drop-shadow-[0_35px_35px_#0007]  rounded-lg' />
-                </div>
-              </Flex>
-            </div>
-            <div className="w-full bg-white rounded-lg p-3">
-              <Flex justifyContent={'space-between'} alignItems={'center'}>
-                <div className="">
-                  <img src={side_banner4} alt="" className='md:h-[350px] h-[200px] max-w-full drop-shadow-[0_35px_35px_#0007]  rounded-lg' />
-                </div>
-                <div className="">
-                  <img src={side_banner6} alt="" className='md:h-[350px] h-[200px] max-w-full drop-shadow-[0_35px_35px_#0007]  rounded-lg' />
-                </div>
-              </Flex>
-            </div>
-          </Slider>
-        </div> */}
       </Flex>
     </section>
   )
 }
 
 {/* <Box bgImage={side_banner3} bgRepeat={'no-repeat'} bgSize={'cover'} bgPos={'center'} className="flex md:h-[410px] h-[250px] justify-center items-center w-full bg_img bg-slate-300">
-              <Flex justifyContent={'center'} alignItems={'center'} height={{md: '410px', base: '250px'}} className="text-white">
-                <Box textAlign={'center'}>
-                  <Heading fontWeight={500} fontSize={30} fontFamily={'cursive'}>WELCOME TO MAAQSIFY</Heading>
-                  <p className=''>We are here to save you time and money</p>
-                </Box>
-              </Flex>
-            </Box> */}
+  <Flex justifyContent={'center'} alignItems={'center'} height={{md: '410px', base: '250px'}} className="text-white">
+    <Box textAlign={'center'}>
+      <Heading fontWeight={500} fontSize={30} fontFamily={'cursive'}>WELCOME TO MAAQSIFY</Heading>
+      <p className=''>We are here to save you time and money</p>
+    </Box>
+  </Flex>
+</Box> */}
